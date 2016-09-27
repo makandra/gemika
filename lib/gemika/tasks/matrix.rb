8f53e2b2170f38faca1efc4b12eaada3c1bcf5fa
Henning Koch <henning.koch@makandra.de>
2016-09-27 16:05:09 +0200

    Remove comment

--- lib/gemika/tasks/matrix.rb
+++ lib/gemika/tasks/matrix.rb
@@ -11,7 +11,6 @@ namespace :matrix do
   desc "Run specs for all Ruby #{RUBY_VERSION} gemfiles"
   task :spec do
     Gemika::Matrix.from_travis_yml.each do |row|
-      # system("bundle exec #{Gemika::Env.rspec_binary} spec")
       rspec_binary = Gemika::Env.rspec_binary_for_gemfile(row.gemfile)
       args = Gemika::Tasks::RSPEC_ARGS
       system("bundle exec #{rspec_binary} #{args}")
