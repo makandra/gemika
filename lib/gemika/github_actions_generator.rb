2f736645a5284b730e358a7f2f420f43be812e6e
Tobias Kraze <tobias.kraze@makandra.de>
2020-10-09 16:26:43 +0200

    fix github workflog generator

--- lib/gemika/github_actions_generator.rb
+++ lib/gemika/github_actions_generator.rb
@@ -1,28 +1,14 @@
 module Gemika
   class GithubActionsGenerator
     TYPES = {
-      test_mysql: {
-        gemfile_filter: /\.mysql/,
-        database_setup: [
-          'sudo apt-get install -y mysql-client libmariadbclient-dev',
-          "mysql -e 'create database IF NOT EXISTS active_type_test;' -u root --password=password -P 3306 -h 127.0.0.1",
-        ],
-        services: {
-          'mysql' => {
-            'image' => 'mysql:5.6',
-            'env' => {
-              'MYSQL_ROOT_PASSWORD' => 'password',
-            },
-            'options' => '--health-cmd "mysqladmin ping" --health-interval 10s --health-timeout 5s --health-retries 5',
-            'ports' => ['5432:5432'],
-          },
-        },
+      test_sqlite: {
+        gemfile_filter: /\.sqlite/,
       },
       test_pg: {
         gemfile_filter: /\.pg/,
         database_setup: [
           'sudo apt-get install -y postgresql-client',
-          "PGPASSWORD=postgres psql -c 'create database active_type_test;' -U postgres -p 5432 -h localhost",
+          "PGPASSWORD=postgres psql -c 'create database test;' -U postgres -p 5432 -h localhost",
         ],
         services: {
           'postgres' => {
@@ -35,8 +21,22 @@ module Gemika
           },
         },
       },
-      test_sqlite: {
-        gemfile_filter: /\.sqlite/,
+      test_mysql: {
+        gemfile_filter: /\.mysql/,
+        database_setup: [
+          'sudo apt-get install -y mysql-client libmariadbclient-dev',
+          "mysql -e 'create database IF NOT EXISTS test;' -u root --password=password -P 3306 -h 127.0.0.1",
+        ],
+        services: {
+          'mysql' => {
+            'image' => 'mysql:5.6',
+            'env' => {
+              'MYSQL_ROOT_PASSWORD' => 'password',
+            },
+            'options' => '--health-cmd "mysqladmin ping" --health-interval 10s --health-timeout 5s --health-retries 5',
+            'ports' => ['3306:3306'],
+          },
+        },
       },
       test: {
         gemfile_filter: //,
@@ -76,15 +76,21 @@ module Gemika
         'name' => 'Install ruby',
         'uses' => 'ruby/setup-ruby@v1',
         'with' => {'ruby-version' => '${{ matrix.ruby }}'},
-      }, {
-        'name' => 'Setup database',
-        'run' => "sudo apt-get install -y postgresql-client\nPGPASSWORD=postgres psql -c 'create database active_type_test;' -U postgres -p 5432 -h localhost\n",
-      }, {
+      }]
+
+      if (database_setup = type_definition[:database_setup])
+        steps << {
+          'name' => 'Setup database',
+          'run' => database_setup.join("\n") + "\n",
+        }
+      end
+
+      steps += [{
         'name' => 'Bundle',
         'run' => "gem install bundler:#{@bundler_version}\nbundle install --no-deployment\n",
       }, {
         'name' => 'Run tests',
-        'run' => 'bundle exec rake spec',
+        'run' => 'bundle exec rspec',
       }]
 
       job = {}
